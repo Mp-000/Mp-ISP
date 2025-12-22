2a37b7afb138b4703ad0819c5642de1539bbd1b1
Mp-000 <tristiantopradita7@gmail.com>
2025-12-22 12:41:25 +0700

    Update storage.js

--- assets/js/storage.js
+++ assets/js/storage.js
@@ -4,3 +4,14 @@ localStorage.setItem(key, JSON.stringify(data));
 function load(key){
 return JSON.parse(localStorage.getItem(key));
 }
+
+// STORAGE HELPER
+function save(key,data){
+localStorage.setItem(key, JSON.stringify(data));
+}
+function load(key){
+return JSON.parse(localStorage.getItem(key));
+}
+function remove(key){
+localStorage.removeItem(key);
+}
