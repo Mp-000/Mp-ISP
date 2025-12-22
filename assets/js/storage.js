c7108e8125d2e56a467650454a6b3d5dd4c8a8f0
Mp-000 <tristiantopradita7@gmail.com>
2025-12-22 12:24:47 +0700

    Update storage.js

--- assets/js/storage.js
+++ assets/js/storage.js
@@ -1,3 +1,20 @@
+// Helper LocalStorage
+function getData(key){
+return JSON.parse(localStorage.getItem(key)) || [];
+}
+function saveData(key, data){
+localStorage.setItem(key, JSON.stringify(data));
+}
+function setSession(user){
+localStorage.setItem('session', JSON.stringify(user));
+}
+function getSession(){
+return JSON.parse(localStorage.getItem('session'));
+}
+function clearSession(){
+localStorage.removeItem('session');
+}
+```javascript
 function getData(key){
 return JSON.parse(localStorage.getItem(key)) || [];
 }
